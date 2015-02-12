62243b5cb7b30eb32154d26758f6398825c14dbd
Colt Frederickson <coltfred@gmail.com>
2015-02-11 17:25:48 -0700

    Add comments and try and eliminate some relatively trivial functions.

--- Data/Humus.hs
+++ Data/Humus.hs
@@ -3,7 +3,7 @@ module Data.Humus (Card(..),
     ManaSymbol(..),
     Printing(..),
     fromName,
-    cardsToCurve,
+    deckToCurve,
     countManaSymbols,
     deckToAvgCMC) where
 
@@ -11,7 +11,9 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.List
 
+--Used for mana symbols and also the color identity of the card.
 data Color = Blue | Black | Red | White | Green deriving (Show, Eq, Ord)
+--Represents the colored mana symbols on the cards.
 newtype ManaSymbol = ManaSymbol { unManaSymbol :: Color } deriving (Eq, Show)
 data Printing = Alpha | Beta deriving (Show, Eq) --Obviously needs more members.
 data Card = Card {  name :: String,
@@ -29,26 +31,29 @@ type Deck = [Card]
 --Map of Cost -> Map (Color -> number of cards)
 type Curve = Map Int (Map Color Int)
 
+--How many of a particular manasymbol exists on the card.
 countManaSymbols :: ManaSymbol -> Card -> Int
 countManaSymbols m = length . filter (/= m) . manaSymbols 
 
-fromName :: String -> Either Error Card
-fromName = error "Not yet implemented"
-
-manaSymbolToColorMap :: ManaSymbol -> Map Color Int
-manaSymbolToColorMap (ManaSymbol c) = Map.singleton c 1
-
-cardsToCurve :: Deck -> Curve
-cardsToCurve = foldl' (Map.unionWith(sum2Maps))(Map.empty) . fmap cardToCurve
+deckToCurve :: Deck -> Curve
+deckToCurve = foldl' (Map.unionWith(sum2Maps))(Map.empty) . fmap cardToCurve
 
 deckToAvgCMC :: Deck -> Float
 deckToAvgCMC deck = 60 / ((foldl (+) 0.0) . fmap (fromIntegral . cmc)) deck
 
+--Convert a single card into a curve for that Card
 cardToCurve :: Card -> Curve
-cardToCurve (Card _ _ m cost _) = Map.singleton cost (sumMaps $ fmap manaSymbolToColorMap m)
+cardToCurve (Card _ _ m cost _) = Map.singleton cost (sumMaps $ fmap ((flip Map.singleton 1) . unManaSymbol) m)
 
+
+--2 general functions for summing up maps. These need to be moved.
 sumMaps :: (Ord k, Num a) => [Map k a] -> Map k a
 sumMaps = foldl' (Map.unionWith(+))(Map.empty)
 
 sum2Maps :: (Ord k, Num a) => Map k a -> Map k a -> Map k a
 sum2Maps first second = sumMaps [first, second]
+
+
+--Parsing stuff goes below here
+fromName :: String -> Either Error Card
+fromName = error "Not yet implemented"
